5ed1ce0a725693a1a9a09082d582b57442fa621f
Alisher <Alisher>
2021-09-02 14:07:55 +0600

    Cards Demo

---
+++ CardsDemo/CardsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsDemoApp.swift
+//  CardsDemo
+//
+//  Created by Алишер Алсейт on 08.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct CardsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardsDemo/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  CardsDemo
+//
+//  Created by Алишер Алсейт on 08.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let transparentColor = Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0)
+    
+    var body: some View {
+        
+        GeometryReader { geo in
+            ZStack {
+                
+                Rectangle()
+                    .foregroundColor(.green)
+                
+                Image("raya")
+                    .resizable()
+                    .scaledToFill()
+                    .offset(x: 0, y: -80)
+                
+                Circle()
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color.blue, transparentColor]), startPoint: .top, endPoint: .bottom))
+                    .frame(width:170)
+                    .offset(x: (geo.size.width - 40)/2 - 20, y: -geo.size.height/2 - 40)
+                    
+                
+                // Content
+                VStack {
+                    
+                    HStack {
+                        Spacer()
+                        Text("Disney+")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                    .padding([.top, .horizontal])
+                    
+                    Spacer()
+                    
+                    ZStack (alignment: .leading) {
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .frame(height: 80)
+                        VStack (alignment: .leading) {
+                            Text("Raya and the Last Dragon")
+                                .font(.headline)
+                            Text("This is a great movie for adults and children alike")
+                                .font(.caption)
+                        }
+                        .padding([.horizontal, .bottom])
+                    }
+                }
+            }
+            .cornerRadius(15)
+            .shadow(radius: 10)
+            .padding()
+        }
+        .frame(height: 500)
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
